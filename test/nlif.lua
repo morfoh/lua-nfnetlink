9e7b125e1a080ef91881e2b12edce93a115f56bf
Christian Wiese <chris@opensde.org>
2012-09-22 22:23:56 +0200

    test/nlif: use index2name to determine all interface names alvailable

--- test/nlif.lua
+++ test/nlif.lua
@@ -6,4 +6,14 @@ print("nlif handle:", nlif)
 print("nlif fd:", nlif:fd())
 print("nlif query:", nlif:query())
 
+print("nlif index2name:")
+local ifidx = -1
+repeat
+	ifidx = ifidx + 1
+	ifname = nlif:index2name(ifidx)
+	if ifname then
+		print(ifidx, ifname)
+	end
+until ifname == nil
+
 nlif:close()
